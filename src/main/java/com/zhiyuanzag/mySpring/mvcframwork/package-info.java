48fc259a9a9fe96f5c4bb39be2c77da155e3d890
zhiyuanzhang9 <zhiyuanzhang9@creditease.cn>
2021-07-11 22:16:55 +0800

    update: 更新package_info

--- src/main/java/com/zhiyuanzag/mySpring/mvcframwork/package-info.java
+++ src/main/java/com/zhiyuanzag/mySpring/mvcframwork/package-info.java
@@ -2,5 +2,6 @@ package com.zhiyuanzag.mySpring.mvcframwork;
 
 /**
  * 功能描述: <br>
- * 〈包中内容为: 利用DispatchServlet实现SpringMVC的基本功能〉
+ * 〈包中内容为: 利用DispatchServlet实现SpringMVC的基本功能〉<br>
+ *     启动类为: {@link com.zhiyuanzag.mySpring.mvcframwork.v1.servlet.ZYDispatcherServlet#init(javax.servlet.ServletConfig)}
 */
--- src/main/java/com/zhiyuanzag/mySpring/springV2/package-info.java
+++ src/main/java/com/zhiyuanzag/mySpring/springV2/package-info.java
@@ -2,5 +2,6 @@ package com.zhiyuanzag.mySpring.springV2;
 
 /**
  * 功能描述: <br>
- * 〈包内主要内容为: 将原有dispatchServlet类中, IoC容器相关内容拆分成对应的类(ApplicationContext)〉
+ * 〈包内主要内容为: 将原有dispatchServlet类中, IoC容器相关内容拆分成对应的类(ApplicationContext)〉<br><br>
+ *  启动类为: {@link com.zhiyuanzag.mySpring.springV2.framework.webmvc.servlet.ZYDispatcherServletV2#init(javax.servlet.ServletConfig)}
 */
